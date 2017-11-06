253feb9c54fc1464303153838bd0378ff732c725
Nguyen Tran <ntran272@csu.fullerton.edu>
2017-11-05 20:27:29 -0800

    updated index

--- formhandler/app/scripts/src/main.js
+++ formhandler/app/scripts/src/main.js
@@ -2,4 +2,4 @@ import FormHandler from './formhandler.js';
 
 let FORM_SELECTOR = '[data-coffee-order="form"]';
 let formHandler = new FormHandler(FORM_SELECTOR);
-formHandler.addSubmitHandler((cb) =>{});
+formHandler.addSubmitHandler((fn) =>{});
