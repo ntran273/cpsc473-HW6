bfbff87fb1dce5aa0b7c7486a942e250c37e9d90
Nguyen Tran <ntran272@csu.fullerton.edu>
2017-11-05 20:19:21 -0800

    updated

--- formhandler/app/scripts/src/formhandler.js
+++ formhandler/app/scripts/src/formhandler.js
@@ -1,41 +1,31 @@
 import $ from 'jquery';
 
-((window) => {
-  'use strict';
-
-  let App = window.App || {};
-  let $ = window.jQuery;
-
-  class FormHandler {
-    constructor(selector) {
-      if (!selector) {
-        throw new Error('No selector provided');
-      }
-
-      this.$formElement = $(selector);
-      if (this.$formElement.length === 0) {
-        throw new Error('Could not find element with selector: ' + selector);
-      }
+class FormHandler {
+  constructor(selector) {
+    if (!selector) {
+      throw new Error('No selector provided');
     }
 
-    addSubmitHandler(fn){
-      console.log('Setting submit handler for form');
-      this.$formElement.on('submit', function(event){
-        event.preventDefault();
-
-        let data = {};
-        $(this).serializeArray().forEach( (item) => {
-          data[item.name] = item.value;
-          console.log(item.name + 'is' + item.name);
-        });
-        console.log(data);
-        fn(data);
-        this.reset();
-        this.elements[0].focus();
-      });
+    this.$formElement = $(selector);
+    if (this.$formElement.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
     }
   }
 
-  App.FormHandler = FormHandler;
-  window.App = App;
-})(window);
+  addSubmitHandler(fn) {
+    console.log('Setting submit handler for form');
+    this.$formElement.on('submit', function(event) {
+      event.preventDefault();
+
+      let data = {};
+      $(this).serializeArray().forEach((item) => {
+        data[item.name] = item.value;
+        console.log(item.name + ' is ' + item.value);
+      });
+      console.log(data);
+      fn(data);
+    });
+  }
+}
+
+export default FormHandler;
